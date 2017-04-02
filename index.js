5b3c88f929a948c3b2aa67a6c2b591a50fb86171
Mike Debnam <mdebnam@gmail.com>
2017-04-02 09:29:15 -0400

    Walk directory and process all files. Fields name, container type, and audio streams.

--- index.js
+++ index.js
@@ -1,52 +1,67 @@
-/* Comment for Hello World */
+/**
+ * Process files in a directory and subdirectories and write audio metadata to a csv file.
+ */
 console.log("Extracting metadata from movie file...");
 
-var fileName = '/Volumes/Movies/Transformers - Dark of the Moon/Transformers - Dark of the Moon-preset.mp4';
-var exec = require('child_process').exec;
-var cmd = 'ffprobe -i "' + fileName + '" -v quiet -print_format json -show_format -show_streams -hide_banner';
+var execSync = require('child_process').execSync;
 
 var fs = require('fs');
+var outputFile = fs.createWriteStream('movies.csv', {defaultEncoding : 'utf8'});
+outputFile.write('Movie Name, Container Type, Audio Streams\n');
+
 var walk = require('walk');
 var files = [];
 
 var walker = walk.walk('/Volumes/Movies', { followLinks: false });
 
 walker.on('file', function(root, stat, next) {
-    // Add this file to the list of files
-
     files.push(root + '/' + stat.name);
     next();
 });
 
 walker.on('end', function() {
     console.log(files);
-});
 
-exec(cmd, function(error, stdout, stderr) {
-	//console.log(stdout);
-
-	fs.appendFile("movies.csv", 'Movie Name, Audio Streams\n');
-
-	var obj = JSON.parse(stdout);
-
-    fs.appendFile("movies.csv", fileName + ',');
-	for (var i = 0; i < obj.streams.length; i++) {
-
-		var stream = obj.streams[i];
-        if (stream.codec_type === 'audio') {
-            console.log(stream.codec_name);
-
-			fs.appendFile("movies.csv", stream.codec_name + ' :: ', function(err) {
-                if (err) {
-                    console.log("EREWRWERWERWER");
-                }
-
-                console.log("File written successfully.");
-            });
-
+    for (var i = 0; i < files.length; i++) {
+        var fileExtension = files[i].split('.').pop();
+        if (fileExtension != 'mp4' && fileExtension != 'mk4') {
+            // This is not a media file, skip.
+            continue;
         }
 
-        fs.appendFile('\n');
-	}
+        var fileName = files[i].split('/').pop().split('.')[0];
 
+        try {
+            outputFile.write(fileName + ',');
+            outputFile.write(fileExtension + ',');
+
+            var cmd = 'ffprobe -i "' + files[i] + '" -v quiet -print_format json -show_format -show_streams -hide_banner';
+            var output = execSync(cmd).toString();
+            processFile(files[i], output);
+        }
+        catch(err) {
+            console.error("Error processing " + files[i] + ":: " + err);
+        }
+
+        outputFile.write('\n');
+    }
+
+    outputFile.end();
 });
+
+
+function processFile(fileName, commandOutput) {
+        //console.log(stdout);
+        console.log("Processing file " + fileName);
+
+        var obj = JSON.parse(commandOutput);
+
+        for (var i = 0; i < obj.streams.length; i++) {
+            var stream = obj.streams[i];
+            if (stream.codec_type === 'audio') {
+                console.log(stream.codec_name);
+                outputFile.write(stream.codec_name + "::");
+            }
+
+        }
+}
