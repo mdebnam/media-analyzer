a3398cf6546c154e447158e70cff379e3c20f588
Mike Debnam <mdebnam@gmail.com>
2017-04-02 14:39:37 -0400

    index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 /**
  * Process files in a directory and subdirectories and write audio metadata to a csv file.
+ *
+ * References:
+ *  node-walk: https://git.daplie.com/Daplie/node-walk
  */
 console.log("Extracting metadata from movie file...");
 
@@ -7,7 +10,7 @@ var execSync = require('child_process').execSync;
 
 var fs = require('fs');
 var outputFile = fs.createWriteStream('movies.csv', {defaultEncoding : 'utf8'});
-outputFile.write('Movie Name, Container Type, Audio Streams\n');
+outputFile.write('Movie Name, Container Type, Audio Streams, Video Codec\r\n');
 
 var walk = require('walk');
 var files = [];
@@ -24,7 +27,7 @@ walker.on('end', function() {
 
     for (var i = 0; i < files.length; i++) {
         var fileExtension = files[i].split('.').pop();
-        if (fileExtension != 'mp4' && fileExtension != 'mk4') {
+        if (fileExtension != 'mp4' && fileExtension != 'mkv') {
             // This is not a media file, skip.
             continue;
         }
@@ -43,7 +46,7 @@ walker.on('end', function() {
             console.error("Error processing " + files[i] + ":: " + err);
         }
 
-        outputFile.write('\n');
+        outputFile.write('\r\n');
     }
 
     outputFile.end();
@@ -51,17 +54,49 @@ walker.on('end', function() {
 
 
 function processFile(fileName, commandOutput) {
-        //console.log(stdout);
-        console.log("Processing file " + fileName);
+    //console.log(commandOutput);
+    console.log("Processing file " + fileName);
 
-        var obj = JSON.parse(commandOutput);
+    var obj = JSON.parse(commandOutput);
 
-        for (var i = 0; i < obj.streams.length; i++) {
-            var stream = obj.streams[i];
-            if (stream.codec_type === 'audio') {
-                console.log(stream.codec_name);
-                outputFile.write(stream.codec_name + "::");
-            }
+    var audioStreams = [];
+    var videoStreams = [];
 
+    // Sort the streams into audio and video
+    for (var i = 0; i < obj.streams.length; i++) {
+        var stream = obj.streams[i];
+        if (stream.codec_type === 'audio') {
+            console.log(stream.codec_name);
+            audioStreams.push(stream);
         }
+
+        if (stream.codec_type === 'video') {
+            console.log(stream.codec_name);
+            videoStreams.push(stream);
+        }
+    }
+
+    // Process the audio streams
+    var audioField = '"';
+    for (var i = 0; i < audioStreams.length; i++) {
+        audioField += audioStreams[i].codec_name + "\r";
+    }
+
+    // Remove the last separator and write this out to our file.
+    audioField = audioField.substring(0, audioField.length - 1);
+    audioField += '"';
+    outputFile.write(audioField);
+
+    // Write the csv field separator
+    outputFile.write(',');
+
+    // Process the video streams
+    var videoField = '';
+    for (var i = 0; i < videoStreams.length; i++) {
+        videoField += videoStreams[i].codec_name + "::";
+    }
+
+    // Remove the last separator and write this out to our file.
+    videoField = videoField.substring(0, videoField.length - 2);
+    outputFile.write(videoField);
 }
